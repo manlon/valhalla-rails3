dc488bda22609413981ec826f153c83db303a994
Matt Hanlon <matthanlon@gmail.com>
2010-03-27 13:38:47 -0700

    Add xapian indexing to the Deed model

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,2 +1,5 @@
 class Deed < ActiveRecord::Base
+  acts_as_xapian :texts => [ :text ], 
+                 :values => [ [ :performed_at, 0, "performed_at", :date ] ], 
+                 :terms => [ [ :speaker, 'S', "speaker" ] ]
 end
